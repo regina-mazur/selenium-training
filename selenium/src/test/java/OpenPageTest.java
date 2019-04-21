d848684dfed9eb4ed66282dd9982aa16342afc17
Regina Mazur <ririmazur@gmail.com>
2019-04-22 01:18:36 +0600

    add first test

---
+++ selenium/src/test/java/OpenPageTest.java
@@ -0,0 +1,33 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class OpenPageTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start(){
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void openPageTest(){
+        driver.get("https://www.seleniumhq.org");
+        driver.findElement(By.id("menu_documentation")).click();
+        wait.until(titleIs("Selenium Documentation — Selenium Documentation"));
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+}
