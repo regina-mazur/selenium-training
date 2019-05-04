5ba87c5a108c44f37ae0a22c558e77bf694dac60
Regina Mazur <ririmazur@gmail.com>
2019-05-05 00:54:45 +0600

    add product stickers test

---
+++ selenium/src/test/java/selenium/CheckProductStickersTest.java
@@ -0,0 +1,22 @@
+package selenium;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import util.PropertyLoader;
+
+import java.util.List;
+
+public class CheckProductStickersTest extends BaseTest {
+    @Test
+    public void checkProductStickersTest(){
+        driver.get(PropertyLoader.loadProperty("homepage.url"));
+
+        List<WebElement> productsList = driver.findElements(By.xpath("//li[@class='product column shadow hover-light']"));
+        for(WebElement product : productsList){
+            Assert.assertEquals(product.findElements(By.xpath(".//div[contains(@class,'sticker')]")).size(),1);
+        }
+    }
+
+}
