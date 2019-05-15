af2e45f3ef7de331d9a8fceb6b3206c4248a6d48
Regina Mazur <ririmazur@gmail.com>
2019-05-16 01:37:05 +0600

    add product test

---
+++ selenium/src/test/java/selenium/AddingProductTest.java
@@ -0,0 +1,82 @@
+package selenium;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import selenium.model.ProductData;
+import util.PropertyLoader;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class AddingProductTest extends BaseTest {
+    private static final String PICTURE_PATH = "./src/test/resources/A097Rr4CEAALo8U.jpg";
+    private static ProductData testData;
+
+    @Before
+    public void setProductData(){
+        testData = new ProductData();
+        testData.setName(UUID.randomUUID().toString().substring(0,5));
+        testData.setCode("Product Code");
+        List<String> categories = new ArrayList<>();
+        categories.add("Subcategory");
+        testData.setCategories(categories);
+        testData.setQuantity(12);
+        testData.setImage(new File(PICTURE_PATH));
+        testData.setDateTo("12.12.2019");
+        testData.setDateFrom("31.12.2019");
+        testData.setManufacturer("1");
+        testData.setKeywords("test test");
+        testData.setShortDescription("product short description");
+        testData.setHeadTitle("Test product title");
+        testData.setPurchasePrice(1200);
+        testData.setCurrency("USD");
+    }
+    @Test
+    public void addingProductTest(){
+        driver.get(PropertyLoader.loadProperty("admin.url"));
+        loginAsAdmin();
+        driver.findElement(By.xpath("//span[contains(text(),'Catalog')]")).click();
+        driver.findElement(By.xpath("//a[contains(text(),'Add New Product')]")).click();
+        fillProductFields(testData);
+        driver.findElement(By.name("save")).click();
+        Assert.assertTrue(driver.findElement(By.xpath("//table[@class='dataTable']//a[text()='" + testData.getName() +"']")).isDisplayed());
+    }
+
+    private void fillProductFields(ProductData newProduct){
+        //General
+        driver.findElement(By.xpath("//label[contains(text(),'Enabled')]")).click();
+        driver.findElement(By.name("name[en]")).sendKeys(newProduct.getName());
+        driver.findElement(By.name("code")).sendKeys(newProduct.getCode());
+        driver.findElement(By.cssSelector("input[data-name='Root']")).click();
+        for (String category: newProduct.getCategories()
+             ) {
+            driver.findElement(By.cssSelector("input[data-name='" + category +"']")).click();
+        }
+        driver.findElement(By.name("quantity")).sendKeys(newProduct.getQuantity().toString());
+        driver.findElement(By.name("new_images[]")).sendKeys(testData.getImage());
+        JavascriptExecutor je = (JavascriptExecutor) driver;
+        je.executeScript("document.querySelector(\"input[name='date_valid_from']\").value=\"" + newProduct.getDateFrom() + "\"");
+        je.executeScript("document.querySelector(\"input[name='date_valid_to']\").value=\"" + newProduct.getDateTo()+ "\"");
+
+        //Info
+        driver.findElement(By.xpath("//a[contains(text(),'Information')]")).click();
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.name("manufacturer_id")));
+        new Select(driver.findElement(By.name("manufacturer_id"))).selectByValue(newProduct.getManufacturer());
+        driver.findElement(By.name("keywords")).sendKeys(newProduct.getKeywords());
+        driver.findElement(By.name("head_title[en]")).sendKeys(newProduct.getHeadTitle());
+
+        //Prices
+        driver.findElement(By.xpath("//a[contains(text(),'Prices')]")).click();
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.name("purchase_price")));
+        driver.findElement(By.name("purchase_price")).sendKeys(newProduct.getPurchasePrice().toString());
+        new Select(driver.findElement(By.name("purchase_price_currency_code"))).selectByValue(newProduct.getCurrency());
+
+    }
+}
---
+++ selenium/src/test/java/selenium/model/ProductData.java
@@ -0,0 +1,142 @@
+package selenium.model;
+
+import java.io.File;
+import java.util.List;
+
+public class ProductData {
+    private String name;
+    private String code;
+    private List<String> categories;
+    private Integer quantity;
+    private String manufacturer;
+    private String supplier;
+    private String keywords;
+    private String shortDescription;
+    private String description;
+    private String headTitle;
+    private Integer purchasePrice;
+    private String currency;
+    private File image;
+    private String dateFrom;
+    private String dateTo;
+
+    public String getDateFrom() {
+        return dateFrom;
+    }
+
+    public void setDateFrom(String dateFrom) {
+        this.dateFrom = dateFrom;
+    }
+
+    public String getDateTo() {
+        return dateTo;
+    }
+
+    public void setDateTo(String dateTo) {
+        this.dateTo = dateTo;
+    }
+
+    public String getImage() {
+        return image.getAbsolutePath();
+    }
+
+    public void setImage(File image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public List<String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<String> categories) {
+        this.categories = categories;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getHeadTitle() {
+        return headTitle;
+    }
+
+    public void setHeadTitle(String headTitle) {
+        this.headTitle = headTitle;
+    }
+
+    public Integer getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(Integer purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
